c5170e0b5ce8e52115096dc03575a2a091ecf9c0
schuettdavid <schuettdavid@gmail.com>
2015-12-19 23:24:42 +1000

    added add test to calculator

---
+++ SimpleFunctions.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleFunctions;
+
+namespace SimpleFunctions.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Add()
+        {
+            Assert.AreEqual(5, Calculator.Add(2, 3));
+        }
+    }
+}
